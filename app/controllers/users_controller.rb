e682e5058a2820181243f81f0979d3377985c16a
t-kehiguchi <ec2-user@ip-172-31-35-85.ap-northeast-1.compute.internal>
2019-10-22 10:00:19 +0000

    ユーザー系の画面(ユーザー一覧及びユーザー編集画面作成、ユーザー詳細画面修正)対応(Dまで完了)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,6 +23,27 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'ユーザー情報の更新に成功しました。'
+      redirect_to user_url @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = 'ユーザー情報の削除に成功しました。'
+    redirect_to users_url
+  end
+
   private
   
     def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017093529) do
+ActiveRecord::Schema.define(version: 20191022083743) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20191017093529) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
